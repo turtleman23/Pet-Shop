f6eb840b0b277a957d2f3135d91ab7e835fa297d
turtleman23 <ryansellers@gmx.com>
2017-06-20 06:26:02 -0600

    bob

--- JavaScript-2.js
+++ JavaScript-2.js
@@ -1,9 +1,30 @@
+var Rats = 1;
+var Level = 0;
 $(document).ready(function() {
     $(".Hungry").hide();
+    while (Rats === 1)
+    {
+        $('.hungry').show();
+    }
 });
 
-var Rat = 0;
-if (Rat === 1)
-{
-    $(".hungry").show();
+function hide() {
+    if (Rats === 1) {
+        $('.Hungry').hide();
+        Rats = 0;
+    }
 }
+
+If (hide().clicked());
+{
+    Level = 100;
+}
+If (Levels > 0); {
+    
+}
+    function Levels() {
+        if (Level === 100)
+        {
+            Level = 0;
+        }
+    }
