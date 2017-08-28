df6c35c8f261efb70b536e9bcba2e48bdf83b556
turtleman23 <ryansellers@gmx.com>
2017-08-27 20:44:27 -0600

    bob

--- JavaScript-2.js
+++ JavaScript-2.js
@@ -1,30 +1,35 @@
-var Rats = 1;
+var Rats = 0;
 var Level = 0;
+var timer = 0;
+var TopLevel = 100;
 $(document).ready(function() {
     $(".Hungry").hide();
-    while (Rats === 1)
-    {
-        $('.hungry').show();
-    }
-});
 
 function hide() {
-    if (Rats === 1) {
+    if (timer < 60) {
         $('.Hungry').hide();
         Rats = 0;
+        Level += 50;
+        console.log(Level);
     }
 }
-
-If (hide().clicked());
-{
-    Level = 100;
-}
-If (Levels > 0); {
-    
-}
-    function Levels() {
-        if (Level === 100)
+function Levels() {
+        if (Level === TopLevel)
         {
             Level = 0;
+            TopLevel += 100;
         }
     }
+    function Hungry() {
+        timer++;
+        if(timer === 60)
+        {
+            $('.Hungry').show();
+            timer = 0;
+        }
+    }
+    setInterval(Hungry).on('click',1000);
+    $('#Hungry').on('click',hide);
+    setInterval(Levels, 1000);
+});
+
